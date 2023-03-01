19d75dd6bb6ef2914408d5182950e6872172892e
Sulaikha Farvin <117733825+sulaikhafarvin@users.noreply.github.com>
2023-03-01 20:08:55 +0530

    Update

--- 1_accounts.js
+++ 1_accounts.js
@@ -1,8 +1,8 @@
 const {ethers} = require("ethers");
 
-const INFURA_ID = '9c35f7b0ffdb4bdabf2dfacf385e31e5';
+const INFURA_ID = 'Infura_id';
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
-const address = '0x08bA21d06DCFd6D011C6Eee7B20EC55aEF792e62';
+const address = 'address';
 
 const main = async() => {
     const balance = await provider.getBalance(address);
