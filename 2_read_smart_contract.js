125e7771c029a5ece6f01d6b38b0bf5391ed52c4
Sulaikha Farvin <117733825+sulaikhafarvin@users.noreply.github.com>
2023-03-01 20:12:13 +0530

    Update

--- 2_read_smart_contract.js
+++ 2_read_smart_contract.js
@@ -1,6 +1,6 @@
 const { ethers } = require("ethers");
 
-const INFURA_ID ='9c35f7b0ffdb4bdabf2dfacf385e31e5';
+const INFURA_ID ='Infura id';
 const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
 
 const ERC20_ABI = [
@@ -9,7 +9,7 @@ const ERC20_ABI = [
     "function totalSupply() view returns (uint256)",
     "function balanceOf(address) view returns (uint)",
 ]
-const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' ;// DAI Contract
+const address = 'address' ;// DAI Contract Address
 
 const contract = new ethers.Contract(address,ERC20_ABI,provider);
 
@@ -24,7 +24,7 @@ const main = async () => {
  console.log(`Symbol: ${symbol}`);
  console.log(`Total supply: ${totalSupply}\n`);
 
- const balance = await contract.balanceOf('0x6B175474E89094C44Da98b954EedeAC495271d0F');
+ const balance = await contract.balanceOf('address');
  console.log(`Balance Returned: ${balance}`);
  console.log(`Balance Formatted: ${ethers.utils.formatEther(balance)}\n`);
 }
